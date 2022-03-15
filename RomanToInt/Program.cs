f673068187ad3744da02063b23612962c6eb094a
Josefine Sjögren <Josefine.sjogren@live.se>
2022-03-15 15:28:06 +0100

    Add project files.

---
+++ RomanToInt/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string romanNumerals = "MDCLXIV";
+
+
+
+Console.WriteLine(RomanToInt.BasicRomanNumerals(Console.ReadLine()));
+
+
---
+++ RomanToInt/RomanToInt.cs
@@ -0,0 +1,58 @@
+﻿
+public class RomanToInt
+{
+
+    public static int BasicRomanNumerals(string str)
+    {
+        int parsedInt = 0;
+        var charArr = str.ToCharArray();
+
+        for (int i = 0; i < charArr.Length; i++)
+        {
+            if (HasNext(charArr, i))
+            {
+                var firstNr = ParseRegularRomanNumeral(charArr[i]);
+                var secondNr = ParseRegularRomanNumeral(charArr[i + 1]);
+
+                if (firstNr >= secondNr)
+                {
+                    parsedInt += firstNr;
+                }
+                else
+                {
+                    parsedInt += (secondNr - firstNr);
+                    i++;
+                }
+            }
+            else
+            {
+                parsedInt += ParseRegularRomanNumeral(charArr[i]);
+            }
+
+        }
+        return parsedInt;
+
+    }
+
+    private static bool HasNext(char[] arr, int i)
+    {
+
+        return arr.Length > i + 1;
+    }
+
+
+    private static int ParseRegularRomanNumeral(char c) =>
+        c switch
+        {
+            'M' => 1000,
+            'D' => 500,
+            'C' => 100,
+            'L' => 50,
+            'X' => 10,
+            'V' => 5,
+            'I' => 1
+        };
+
+
+}
+
